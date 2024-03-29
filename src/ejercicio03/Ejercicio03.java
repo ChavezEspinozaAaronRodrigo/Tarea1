47dec98ab48ef4fa5d23f2a2c5c5faf1730ba1ce
Aaron <Aaron@DESKTOP-A25L3CV>
2024-03-29 16:46:08 -0500

    primercomit

---
+++ src/ejercicio03/Ejercicio03.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio03;
+
+/**
+ *
+ * @author Aaron
+ */
+public class Ejercicio03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int entero=6;
+        
+        double decimal=6.0;
+        
+        String texto="Carlos Palomino";
+        
+        char caracter='X';
+        
+        System.out.print("Valor de la variable entera: ");
+        System.out.println(entero);
+        
+        System.out.print("valora de la variable doble:" );
+        System.out.println(decimal);
+        
+        System.out.print("Valor de la variable string: ");
+        System.out.println(texto);
+        
+        System.out.print("Valor de la variable char: ");
+        System.out.println(caracter);
+    }
+    
+}
