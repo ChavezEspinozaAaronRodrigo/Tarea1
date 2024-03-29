966d3cf3ed9df997422da823c0815fbea530ee0a
Aaron <Aaron@DESKTOP-A25L3CV>
2024-03-29 16:49:00 -0500

    primercomit

---
+++ src/ejercicio04/Ejercicio04.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio04;
+
+/**
+ *
+ * @author Aaron
+ */
+public class Ejercicio04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String texto1="Carlos";
+        String texto2="Palomino";
+        
+        final double decimal=6.6;
+        final double decimal2=6.8;
+                
+        System.out.printf("Valor del primer string: %s\n", texto1);
+        System.out.printf("Valor del segundo string: %s\n", texto2);
+        System.out.printf("Valor del primer decimal double: %.5f\n", decimal);
+        System.out.printf("Valor del primer decimal double: %.5f\n", decimal2);        
+    }
+    
+}
