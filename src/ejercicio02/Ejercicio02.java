67c76224ade54c1ae6f308f71a63ad2035bd3e36
Aaron <Aaron@DESKTOP-A25L3CV>
2024-03-29 16:42:16 -0500

    primercomit

---
+++ src/ejercicio02/Ejercicio02.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio02;
+
+/**
+ *
+ * @author Aaron
+ */
+public class Ejercicio02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here}
+        int entero1=5;
+        int entero2=10;
+        int entero3=15;
+        
+        double entero4=5.5;
+        double entero5=10.5;
+        double entero6=15.5;
+        
+        String entero7="Programar";
+        String entero8="Taller";
+        String entero10="UTP";
+        
+        char entero11='A';
+        char entero12='B';
+        char entero13='C';
+        
+        System.out.println("Enteros");
+        System.out.println("Entero 1: "+entero1);
+        System.out.println("Entero 2: "+entero2);
+        System.out.println("Entero 3: "+entero3);
+        
+        System.out.println("\nDoubles:");
+        System.out.println("Double 1: "+entero4);
+        System.out.println("Double 2: "+entero5);
+        System.out.println("Double 3: "+entero6);
+        
+         System.out.println("\nStrings:");
+        System.out.println("String 1: "+entero7);
+        System.out.println("String 2: "+entero8);
+        System.out.println("String 3: "+entero10);
+        
+        System.out.println("\nChars:");
+        System.out.println("Char 1: "+entero11);
+        System.out.println("Char 2: "+entero12);
+        System.out.println("Char 3: "+entero13);
+
+    }
+    
+}
